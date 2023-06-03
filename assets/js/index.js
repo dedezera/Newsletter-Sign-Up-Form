61bb60b1657e02afc1d31616eef08ab8fd9f6661
André Felipe <doro123andre@gmail.com>
2023-06-03 13:10:28 -0300

    fix mail input empty

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,13 +3,22 @@ const subscribed = document.querySelector(".subscribed")
 const submitBtn = document.querySelector("#btn")
 const resubmitBtn = document.querySelector(".sub-btn")
 const mailSpan = document.querySelector(".userMail")
-
+const userMailNone = document.querySelector(".valid-mail")
+const formInput = document.querySelector("#mail") 
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault()
     const mailInput = document.querySelector("#mail")
     const userMail = mailInput.value
+
     mailSpan.textContent = userMail
+
+    if (!userMail) {
+        userMailNone.classList.remove("hide")
+        formInput.style.backgroundColor = "#FFE8E6"
+        formInput.style.borderColor = "#ff6257"
+        return
+    }
 
     subscribed.classList.remove("hide")
     mainContainer.style.display = "none"
